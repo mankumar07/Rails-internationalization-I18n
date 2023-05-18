32d079e4f4fbf0e4aa8ce8b9ad600e5048ffd43a
mankumar07 <mankumar@bestpeers.com>
2023-05-18 17:16:24 +0530

    second commit

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative "boot"
 
 require "rails/all"
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -18,7 +17,7 @@ module Internationalization
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.i18n.available_locales = [:en, :ru]
+    config.i18n.available_locales = [:en, :ru, :hi]
     config.i18n.default_locale = :en
   end
 end
